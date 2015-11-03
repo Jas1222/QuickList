6fb222bdd5f34d74d04eaee975e7edff4c947162
Jas1222 <jaspalhayer@Jaspals-MacBook-Pro.local>
2015-11-03 19:15:32 +0000

    inserting records to database now works

--- app/src/main/java/com/example/jaspalhayer/quicklist/ConnectionHandler.java
+++ app/src/main/java/com/example/jaspalhayer/quicklist/ConnectionHandler.java
@@ -1,14 +1,37 @@
 package com.example.jaspalhayer.quicklist;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+import org.json.JSONException;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Created by jaspalhayer on 29/10/2015.
@@ -16,8 +39,27 @@ import java.net.URL;
 public class ConnectionHandler {
     HttpURLConnection urlConnection;
     StringBuilder result = new StringBuilder();
+    String postUrl = "http://qt003605.webs.sse.reading.ac.uk/android_connect/create_book_listingTest.php";
 
-    // Reads listings
+    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException{
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(Map.Entry<String, String> entry : params.entrySet()){
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+
+        return result.toString();
+    }
+
+
+    // TODO Rename class to read
     public String openHttpConnection() {
         try {
 
@@ -52,4 +94,40 @@ public class ConnectionHandler {
         return result.toString();
     }
 
+    public void createListingPost(Context context){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, postUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        System.out.println(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                       System.out.println(error);
+                    }
+                }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("book_title",CreateListingActivity.bookInfoField);
+                params.put("book_author",CreateListingActivity.authorInfoField);
+                params.put("book_year",CreateListingActivity.yearInfoField);
+                params.put("book_desc",CreateListingActivity.descInfoField);
+                params.put("book_price", CreateListingActivity.priceInfoField);
+                return params;
+            }
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(stringRequest);
+    }
+
+    public void saveParams() {
+
+    }
 }
+
+
--- app/src/main/java/com/example/jaspalhayer/quicklist/CreateListingActivity.java
+++ app/src/main/java/com/example/jaspalhayer/quicklist/CreateListingActivity.java
@@ -1,13 +1,32 @@
 package com.example.jaspalhayer.quicklist;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by jaspalhayer on 03/11/2015.
  */
 public class CreateListingActivity extends AppCompatActivity {
+
+    EditText titleText;
+    EditText authorText;
+    EditText yearText;
+    EditText descText;
+    EditText priceText;
+
+    Button mListing;
+
+    public static String bookInfoField;
+    public static String authorInfoField;
+    public static String yearInfoField;
+    public static String descInfoField;
+    public static String priceInfoField;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,9 +36,31 @@ public class CreateListingActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        titleText = (EditText)findViewById(R.id.titleText);
+        authorText = (EditText)findViewById(R.id.authorText);
+        yearText = (EditText)findViewById(R.id.yearText);
+        descText = (EditText)findViewById(R.id.descText);
+        priceText = (EditText)findViewById(R.id.priceText);
 
-        // ** OPENS CONNECTION TO DATABASE TO RETRIEVE ALL LISTINGS - TODO MOVE
-        //new LoadAllListings().execute();
+        mListing = (Button)findViewById(R.id.makeListingBtn);
+        mListing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Launching All products Activity
+                bookInfoField = titleText.getText().toString();
+                authorInfoField = authorText.getText().toString();
+                yearInfoField = yearText.getText().toString();
+                descInfoField = descText.getText().toString();
+                priceInfoField = priceText.getText().toString();
+
+                new CreateBookListing().execute();
+
+            }
+        });
+
+
+
+
 
     }
 
@@ -28,5 +69,27 @@ public class CreateListingActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.create_listing_toolbar, menu);
         return true;
+    }
+
+    // ** Opens connection to database to load listings, IS NOT NEEDED ATM **
+    class CreateBookListing extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        protected String doInBackground(String... args) {
+            ConnectionHandler handler = new ConnectionHandler();
+            try {
+                handler.createListingPost(getApplicationContext());
+            } catch (Exception e) {
+                System.out.println("Unable to create listing");
+            }
+            return null;
+        }
     }
 }
