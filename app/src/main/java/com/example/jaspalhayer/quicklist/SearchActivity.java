98ef0188f77926e43e33d1ee14412f617db52900
Jas1222 <jaspalhayer@Jaspals-MacBook-Pro.local>
2015-11-27 12:49:05 +0000

    finished searchUi

--- app/src/main/java/com/example/jaspalhayer/quicklist/SearchActivity.java
+++ app/src/main/java/com/example/jaspalhayer/quicklist/SearchActivity.java
@@ -3,9 +3,9 @@ package com.example.jaspalhayer.quicklist;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class SearchActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
