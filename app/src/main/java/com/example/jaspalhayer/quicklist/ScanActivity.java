1b8d9b8f21d6d72612d8cef010cf60f2023f932e
Jaspal Hayer <jaspal@homepc.com>
2016-01-05 22:12:58 +0000

    ease enter the commit message for your changes. Lines starting

---
+++ app/src/main/java/com/example/jaspalhayer/quicklist/ScanActivity.java
@@ -0,0 +1,14 @@
+package com.example.jaspalhayer.quicklist;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ScanActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+    }
+
+}
---
+++ app/src/main/java/com/google/zxing/integration/IntentIntegrator.java
@@ -0,0 +1,7 @@
+package com.google.zxing.integration;
+
+/**
+ * Created by Jas1222 on 05/01/2016.
+ */
+public class IntentIntegrator {
+}
