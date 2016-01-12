e24a78e015e5a7fc3ccb6a7da2d7466e13ce2c9a
Jas1222 <jaspalhayer@Jaspals-MacBook-Pro.local>
2016-01-12 19:18:48 +0000

    safety commit

--- app/src/main/java/com/example/jaspalhayer/quicklist/CreateListingActivity.java
+++ app/src/main/java/com/example/jaspalhayer/quicklist/CreateListingActivity.java
@@ -53,19 +53,6 @@ public class CreateListingActivity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.create_main_container, createFragment1);
         fragmentTransaction.commit();
 
-
-//        setVariablesToUiElements();
-//        createArrayAdapters();
-//        setSpinnerToAdapter();
-//        setAdapter();
-
-//        mListing.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                saveUserInputText();
-//                new CreateBookListing().execute();
-//            }
-//        });
     }
 
     @Override
@@ -80,69 +67,4 @@ public class CreateListingActivity extends AppCompatActivity {
         //getMenuInflater().inflate(R.menu.create_listing_toolbar, menu);
         return true;
     }
-/*
-    private void saveUserInputText(){
-        bookInfoField = titleText.getText().toString();
-        authorInfoField = authorText.getText().toString();
-        yearInfoField = yearText.getText().toString();
-        descInfoField = descText.getText().toString();
-        priceInfoField = priceText.getText().toString();
-    }
-
-    private void setVariablesToUiElements(){
-        titleText = (EditText)findViewById(R.id.titleText);
-        authorText = (EditText)findViewById(R.id.authorText);
-        yearText = (EditText)findViewById(R.id.yearText);
-        descText = (EditText)findViewById(R.id.descText);
-        priceText = (EditText)findViewById(R.id.priceText);
-        lstView = (ListView)findViewById(R.id.courseList);
-
-        degreeTypeSpinner = (Spinner)findViewById(R.id.degreeTypeSpinner);
-        courseSpinner = (Spinner)findViewById(R.id.courseSpinner);
-        yearSpinner = (Spinner)findViewById(R.id.yearSpinner);
-        mListing = (Button)findViewById(R.id.makeListingBtn);
-    }
-
-    protected void createArrayAdapters(){
-        degreeTypeAdapter = ArrayAdapter.createFromResource(this, R.array.course_type, R.layout.course_spinner_layout_create);
-        ugCourseAdapter = ArrayAdapter.createFromResource(this, R.array.undergrad_courses, R.layout.course_spinner_layout_create);
-        pgCourseAdapter = ArrayAdapter.createFromResource(this, R.array.postgrad_courses, R.layout.course_spinner_layout_create);
-        yearAdapter = ArrayAdapter.createFromResource(this, R.array.course_year_array, R.layout.course_spinner_layout_create);
-      //  initialCourseAdapter = ArrayAdapter.createFromResource(this, R.array.initial_course, R.layout.course_spinner_layout);
-    }
-
-    protected void setSpinnerToAdapter(){
-        ugCourseAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        pgCourseAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        yearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        degreeTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-      //  initialCourseAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    }
-
-    protected void setAdapter(){
-        yearSpinner.setAdapter(yearAdapter);
-        courseSpinner.setAdapter(ugCourseAdapter);
-        degreeTypeSpinner.setAdapter(degreeTypeAdapter);
-    }*/
-
-//    class CreateBookListing extends AsyncTask<String, String, String> {
-//
-//        /**
-//         * Before starting background thread Show Progress Dialog
-//         */
-//        @Override
-//        protected void onPreExecute() {
-//            super.onPreExecute();
-//        }
-//
-//        protected String doInBackground(String... args) {
-//            ConnectionHandler handler = new ConnectionHandler();
-//            try {
-//                handler.createListingPost(getApplicationContext());
-//            } catch (Exception e) {
-//                System.out.println("Unable to create listing");
-//            }
-//            return null;
-//        }
-//    }
 }
