95c30a25a5a96e955dbb9aa481bcff6d29ea2cad
Jas1222 <jaspalhayer@Jaspals-MacBook-Pro.local>
2016-02-09 22:00:51 +0000

    user can login and json is sent back

--- app/src/main/java/com/example/jaspalhayer/quicklist/LoginFragment.java
+++ app/src/main/java/com/example/jaspalhayer/quicklist/LoginFragment.java
@@ -1,13 +1,29 @@
 package com.example.jaspalhayer.quicklist;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +33,9 @@ public class LoginFragment extends Fragment {
     EditText loginEmailField;
     EditText loginPasswordField;
     CardView mLoginButton;
+    LoginButton facebookLoginButton;
+    CallbackManager callbackManager;
+
 
     OnLoginCallback mCallback;
 
@@ -26,15 +45,26 @@ public class LoginFragment extends Fragment {
         // Required empty public constructor
     }
 
-    public interface OnLoginCallback{
+    public interface OnLoginCallback {
         void onLoginSuccess();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View rootView = inflater.inflate(R.layout.activity_login, container, false);
         setVariablesToUiElements(rootView);
+        facebookLoginButton.setReadPermissions("public_profile, email");
+        facebookLoginButton.setFragment(this);
 
         mCallback = (OnLoginCallback) getActivity();
 
@@ -53,14 +83,57 @@ public class LoginFragment extends Fragment {
         return rootView;
     }
 
-    private void setVariablesToUiElements(View rootView){
-        loginEmailField = (EditText)rootView.findViewById(R.id.login_emailField);
-        loginPasswordField = (EditText)rootView.findViewById(R.id.login_passwordField);
-        mLoginButton = (CardView)rootView.findViewById(R.id.login_login_button);
+    private void setVariablesToUiElements(View rootView) {
+        loginEmailField = (EditText) rootView.findViewById(R.id.login_emailField);
+        loginPasswordField = (EditText) rootView.findViewById(R.id.login_passwordField);
+        mLoginButton = (CardView) rootView.findViewById(R.id.login_login_button);
+        facebookLoginButton = (LoginButton) rootView.findViewById(R.id.facebook_login_button);
     }
 
-    private void saveUserInputText(UserCredentialHandler loginHandler){
+    private void saveUserInputText(UserCredentialHandler loginHandler) {
         loginHandler.userEmail = loginEmailField.getText().toString();
         loginHandler.userPassword = loginPasswordField.getText().toString();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        facebookLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                System.out.println(loginResult);
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(
+                                    JSONObject object,
+                                    GraphResponse response) {
+                                // Application code
+                                Log.v("LoginActivity", response.toString());
+                                // Possibly run callback onsuccess?
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+                Log.v("LoginActivity", "cancel");
+                System.out.println("attempted canceled");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                System.out.println("attempted failed");
+                Log.v("LoginActivity", exception.getCause().toString());
+            }
+        });
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
